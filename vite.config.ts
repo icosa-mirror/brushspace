fa96f7e3b09565aa343b50bab8c76d0a6b194906
Andy Baker <andy@andybak.net>
2026-08-20 19:41:51 +0100

    Make local HTTPS opt-in

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig(({ mode }) => ({
   plugins: [
-    ...(mode === "test" || mode.endsWith("http") ? [] : [mkcert()]),
+    ...(mode.endsWith("https") ? [mkcert()] : []),
     iwsdkDev({
       emulator: {
         device: "metaQuest3",
